3c3002cfba7a0aaef216895d5fb0b90d7c8dd9e2
Ripleonardo000 <leonardoandrade2003.197@gmail.com>
2024-05-22 12:16:36 -0500

    Button clicked

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,6 +12,7 @@ namespace LeonardoAndradeAPITaller
             InitializeComponent();
         }
 
+        /*
         private void Button_Clicked(object sender, EventArgs e)
         {
             var client = new HttpClient();
@@ -22,6 +23,22 @@ namespace LeonardoAndradeAPITaller
                 var burgers = response.Content.ReadAsStringAsync().Result;
                 var burgersList =
                JsonConvert.DeserializeObject<List<Burger>>(burgers);
+                listView.ItemsSource = burgersList;
+            }
+        }
+        */
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7119/api/");
+            var response = client.GetAsync("burger").Result;
+            if(response.IsSuccessStatusCode)
+            {
+
+                var burgers= response.Content.ReadAsStringAsync().Result;
+                var burgersList =
+                    JsonConvert.DeserializeObject<List<Burger>>(burgers);
                 listView.ItemsSource = burgersList;
             }
         }
