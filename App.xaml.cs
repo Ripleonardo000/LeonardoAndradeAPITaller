a69e4f962e3c5964b8563ec3a01e1e72b2d3d1df
Ripleonardo000 <leonardoandrade2003.197@gmail.com>
2024-05-22 11:31:09 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LeonardoAndradeAPITaller
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Models/Burger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeonardoAndradeAPITaller.Models
+{
+
+    /*
+    public class Burger
+    {
+        public int burgerId { get; set; }
+        public string name { get; set; }
+        public bool withCheese { get; set; }
+        public int costo { get; set; }
+        public List<object> promos { get; set; }
+    }
+    */
+
+    
+    public class Burger
+    {
+        public int burgerId { get; set; }
+        public string? name { get; set; }
+        public bool withCheese { get; set; }
+        public decimal costo { get; set; }
+        public object[]? promos { get; set; }
+    }
+    
+
+
+
+}
